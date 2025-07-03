a97c39cbb55ba0d4060e840d1d249f7cce8d82bb
alfitosantosa <sanskun02pg@gmail.com>
2025-07-03 11:55:04 +0700

    push /healty

--- routes/upload.route.js
+++ routes/upload.route.js
@@ -7,5 +7,11 @@ router.post("/upload", upload.single("file"), uploadController.uploadFile);
 router.get("/files/:filename", uploadController.getFile);
 router.get("/files", uploadController.listFiles);
 router.delete("/files/:filename", uploadController.deleteFile);
+router.get("/health", (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "Server is healthy",
+  });
+});
 
 module.exports = router;
